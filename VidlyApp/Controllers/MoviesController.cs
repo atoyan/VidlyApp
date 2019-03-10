26e00683f0fe4242cda3a3c1d5b28315e1a3786a
Andranik Atoyan <atoyanandranik@gmail.com>
2019-03-10 14:29:38 +0100

    initial commit

---
+++ VidlyApp/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using VidlyApp.Models;
+
+namespace VidlyApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            
+            
+            var movie = new Movie() {Name =  "Shrek"};
+            
+            return View(movie); 
+      
+        }
+
+        public ActionResult ByReleaseDate(int year, int month){
+            return Content(year+"/" + month);
+
+    }
+              
+    
+    }
+}
